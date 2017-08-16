01ea6c321d0c33375fd1e40ce5cd190962081c37
Jason Kirby <jasonk@vdsys.com>
2017-08-16 17:36:36 -0600

    Added routing to the routers-start project

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ActivatedRoute, Params, Router, Data } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -17,18 +17,24 @@ export class ServerComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    const id = +this.route.snapshot.params['id'];
-    this.server = this.serversService.getServer(id);
-    this.route.params
-      .subscribe(
-        (params: Params) => {
-          this.server = this.serversService.getServer(+params['id']);
+   this.route.data
+     .subscribe(
+        (data: Data)=>{
+            this.server = data['server'];
         }
-      );
+     );
+   // const id = +this.route.snapshot.params['id'];
+   // this.server = this.serversService.getServer(id);
+   // this.route.params
+   //   .subscribe(
+   //     (params: Params) => {
+   //       this.server = this.serversService.getServer(+params['id']);
+   //     }
+   //   );
   }
   
   onEdit(){
-    this.router.navigate(['/servers', this.server['id'], 'edit'], {queryParams: {allowEdit: '1'}, fragment: 'loading'});
+    this.router.navigate(['edit'], {relativeTo: this.route, queryParamsHandling: 'preserve'});
   }
 
 }
